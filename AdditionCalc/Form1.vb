6575877a5fe3cd311b702e88f6470e47909f0d28
rextencydemo <bradley.martel@rextencytech.com>
2015-03-18 10:54:26 -0400

    addition calc with added hooks for ali

---
+++ AdditionCalc/Form1.vb
@@ -0,0 +1,11 @@
+﻿Public Class Form1
+
+    Dim a, b, r As Integer
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        a = Val(TextBox1.Text)
+        b = Val(TextBox2.Text)
+        r = a + b
+        TextBox3.Text = r
+    End Sub
+End Class
