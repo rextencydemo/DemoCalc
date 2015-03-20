6c74b08bed47378795939ffaf03f3c9e94972c31
rextencydemo <bradley.martel@rextencytech.com>
2015-03-20 10:43:24 -0400

    try 2

--- AdditionCalc/Form1.vb
+++ AdditionCalc/Form1.vb
@@ -5,7 +5,7 @@
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         a = Val(TextBox1.Text)
         b = Val(TextBox2.Text)
-        r = a * b
+        r = a + b
         TextBox3.Text = r
     End Sub
 End Class
